ee53e8eb4d28b84b136cb9cfa4fb26c7f87aabae
ahmedazad <ahmedazad750@gmail.com>
2022-06-27 17:50:40 +0300

    animation - last update

---
+++ startPage.js
@@ -0,0 +1,12 @@
+const background = document.querySelector(".start");
+const text = document.querySelector(".start h1");
+
+// move the background up
+setTimeout(() => {
+  background.style.top = "-100%";
+}, 1000);
+
+// move text down and rotate
+setTimeout(() => {
+  text.style.transform = "translateY(200%) rotate(90deg)";
+}, 1000);
