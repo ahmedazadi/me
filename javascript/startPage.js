fbf0109a25d8ea8c89c4771ff4a84674a1fc09ff
ahmedazad <ahmedazad750@gmail.com>
2022-06-28 10:46:05 +0300

    fix background stick to top - projects go back

--- javascript/startPage.js
+++ javascript/startPage.js
@@ -6,6 +6,10 @@ setTimeout(() => {
   background.style.top = "-100%";
 }, 1000);
 
+setTimeout(() => {
+  background.style.display = "none";
+}, 2000);
+
 // move text down and rotate
 setTimeout(() => {
   text.style.transform = "translateY(200%) rotate(90deg)";
